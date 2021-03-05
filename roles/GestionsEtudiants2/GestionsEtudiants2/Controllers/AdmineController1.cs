a746a7c80da8bfd70816159d0ac0357cb16029c0
jihad <jihad.karamane91@gmail.com>
2021-03-05 15:10:33 +0100

    commit-3

---
+++ roles/GestionsEtudiants2/GestionsEtudiants2/Controllers/AdmineController1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionsEtudiants2.Controllers
+{
+    public class AdmineController1 : Controller
+    {
+        [Authorize (Roles ="Admine")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
